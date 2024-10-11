79b0e11e836cf80228844f9502b8402f59bf26ad
Vitor Martins <vitormartinsweb@gmail.com>
2024-10-11 10:15:50 +0100

    feat: keep styling

--- parceljs/tailwind.config.js
+++ parceljs/tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
       colors: {
         'lp-purple-300': '#37113e',
         'lp-purple-100': '#6c0082',
+        'lp-pink-100': '#fe96f5',
         'lp-pastel-100': "#fff9f3",
         'lp-green-100': '#85f753',
       }
